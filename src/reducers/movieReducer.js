4bda15c0d845b1dc143955354ce4546dd2ebaadf
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-18 18:28:24 -0500

    Connected the delete and Add Movie actions

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -12,6 +12,11 @@ const reducer = (state = initialState, action) => {
             return {
                 movies: state.movies.filter(item=>(action.payload !== item.id))
             }
+        case ADD_MOVIE:
+            return {
+                ...state,
+                movies: [...state.movies, {...action.payload, id: state.movies.length}]
+            }
         default:
             return state;
     }
